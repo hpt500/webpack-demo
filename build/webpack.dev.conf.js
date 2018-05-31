581ea58fc686b837248926cb9e9894add38e9190
hpt500 <565420423@qq.com>
2018-05-31 17:56:33 +0800

    配置更新

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -12,7 +12,7 @@ const config = merge(base,{
         overlay: true, // 浏览器页面上显示错误
         open: true, // 开启自动打开浏览器
         // stats: "errors-only", //stats: "errors-only"表示只打印错误：
-        hot: true // 开启热更新
+        // hot: true // 开启热更新
     },
 })
 
