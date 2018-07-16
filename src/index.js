21859d95b4e7fb73af49b326bfe3bc8ff90f9bb1
hpt500 <565420423@qq.com>
2018-07-16 17:50:22 +0800

    基本项目配置更新

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import './scss/style.scss'
 function component() {
     var element = document.createElement('div');
     
-        // Lodash, now imported by this script
-        element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-        element.classList.add('hello');
-        element.classList.add('helloImg');
-        element.onclick = function(){
-            this.classList.toggle('hoho')
-        }
-        return element;
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+    element.classList.add('helloImg');
+    element.onclick = function(){
+        this.classList.toggle('hoho')
     }
+    return element;
+}
   
 document.body.appendChild(component());
